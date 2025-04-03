cbaca1d1cf20da541b5cb56cff42063ffdfd4a30
venumadhav17 <kalyanamvenumadhav@gmail.com>
2025-04-03 11:51:17 +0530

    Backend done for chat app

--- src/index.ts
+++ src/index.ts
@@ -13,20 +13,22 @@ wss.on("connection", (socket) => {
   socket.on("message", (message) => {
     // string convert into an object
     const parsedMessage = JSON.parse(message as unknown as string);
-    console.log(parsedMessage); // object
-    if (parsedMessage.type === "join") {
+    // object
+    if (parsedMessage.type == "join") {
+      console.log("user joined room " + parsedMessage.payload.roomId);
       allSockets.push({
         socket,
         room: parsedMessage.payload.roomId
       });
     }
 
-    if (parsedMessage.type === "chat") {
+    if (parsedMessage.type == "chat") {
       //const currentUserRoom = allSockets.find((x) =>x.socket == socket).room
       let currentUserRoom = null; // {}
       for (let i = 0; i < allSockets.length; i++) {
         if (allSockets[i].socket == socket) {
           currentUserRoom = allSockets[i].room;
+          allSockets[i].socket.send(parsedMessage.payload.message);
         }
       }
 
